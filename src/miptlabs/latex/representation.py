4139e6704b2c76fa86276ce34a00ed31055f0b3d
Dmitry <dimansharonov95@gmail.com>
2022-05-19 02:36:08 +0300

    Фикс расчета процентов при форматировании значений с погрешностью в латех

--- src/miptlabs/latex/representation.py
+++ src/miptlabs/latex/representation.py
@@ -63,7 +63,7 @@ def represent_value(name, val=None, sigma=None, eps=None, units='', n=-1):
 
     else:
         val_ = val
-        sigma_ = sigma * 100
+        sigma_ = sigma
         eps_ = eps * 100
 
     return f'$ {name} = {val_} \\pm {sigma_}{units}, \\; \\varepsilon_{{{name}}} = {eps_} \\% $'
